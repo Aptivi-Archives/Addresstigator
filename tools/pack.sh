93be295cb03e3701fde1e1e4a7c1e1314657cdf8
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-10-29 14:13:17 +0300

    und - Update build system to use ZIP

--- tools/pack.sh
+++ tools/pack.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-# This script builds and packs the artifacts. Use when you have MSBuild installed.
+# This script builds KS and packs the artifacts. Use when you have MSBuild installed.
 version=$(cat version)
-releaseconf=$1
-if [ -z $releaseconf ]; then
-	releaseconf=Release
-fi
 
 # Check for dependencies
-rarpath=`which rar`
+zippath=`which zip`
 if [ ! $? == 0 ]; then
-	echo rar is not found.
+	echo zip is not found.
 	exit 1
 fi
 
 # Pack binary
 echo Packing binary...
-"$rarpath" a -ep1 -r -m5 /tmp/$version-bin.rar "../Addresstigator/bin/$releaseconf/netstandard2.0/"
-"$rarpath" a -ep1 -r -m5 /tmp/$version-demo.rar "../Addresstigator.Console/bin/$releaseconf/net6.0/"
+cd "../Addresstigator/bin/$releaseconf/netstandard2.0/" && "$zippath" -r /tmp/$version-bin.zip . && cd -
+cd "../Addresstigator.Console/bin/$releaseconf/net6.0/" && "$zippath" -r /tmp/$version-demo.zip . && cd -
 if [ ! $? == 0 ]; then
-	echo Packing using rar failed.
+	echo Packing failed.
 	exit 1
 fi
 
 # Inform success
-mv ~/tmp/$version-bin.rar .
-mv ~/tmp/$version-demo.rar .
+mv /tmp/$version-bin.zip .
+mv /tmp/$version-demo.zip .
 echo Build and pack successful.
 exit 0
